7ee0a2be2126bd0f2a487f351bb1482413eafc56
qPCR4vir <arielvina@yahoo.es>
2016-07-11 11:03:09 +0200

    add FOREIGN KEY references, bio_seq, GB_seq, etc.

--- data/python/seq_sql.py
+++ data/python/seq_sql.py
@@ -13,82 +13,109 @@ def create() -> sqlite3.Connection:
     # seq_file
     c.execute("""CREATE TABLE   seq_file
                      (
-                       Id     INTEGER PRIMARY KEY AUTOINCREMENT,
-                       path   TEXT    ,
-                       format TEXT,
-                     )
+                       Id_file   INTEGER PRIMARY KEY AUTOINCREMENT,
+                       path      TEXT    ,
+                       format    TEXT,
+                     );
               """)
 
-    # seq
+    # seq    -- Any "experimental" sequence, for example: most GenBank entries (the sequence part).
+    #           but also sequences obtained in the lab.
     c.execute("""CREATE TABLE   seq
                      (
-                       Id    INTEGER PRIMARY KEY AUTOINCREMENT,
-                       Name  TEXT    UNIQUE,
-                       Seq   TEXT,                  -- The original, preferable full sequence
-                       gen   INTEGER NOT NULL,      -- Genomic region (CG, ORF1, etc.)
-                       Len   INT,
-                       file  INTEGER,
+                       Id_seq   INTEGER PRIMARY KEY AUTOINCREMENT,
+                       Name     TEXT    UNIQUE,
+                       Seq      TEXT,                  -- The original, preferable full sequence
+                       Id_gen   INTEGER NOT NULL REFERENCES genomic_region,   --  (CG, ORF1, etc.)
+                       Len      INT,
+                       Id_file  INTEGER REFERENCES seq_file,
                        file_beg INT,
                        file_end INT,
-                       time  INT
-                     )
+                       time     INT
+                     );
               """)
 
-    # seq_fragment
+    # bio_seq  -- A complete sequence from a gen, organism, etc. For example the HEV complete genomes.
+    # temp: just a seq with Id_gen from a genomic_region with Name "CG".
+    c.execute("""CREATE TABLE   bio_seq
+           (
+             Id_bio_seq       INTEGER PRIMARY KEY AUTOINCREMENT,
+             Id_taxa          TEXT    UNIQUE,
+             description      TEXT,
+             locus            TEXT,
+             Id_seq           INTEGER   REFERENCES seq
+           );
+    """)
+
+    # GB_seq  -- a simplified view of a GenBank entry sequence: todo use BioSQL
+    c.execute("""CREATE TABLE   GB_seq
+           (
+             Id_GB_seq        INTEGER PRIMARY KEY AUTOINCREMENT,
+             Acc              TEXT    UNIQUE,
+             description      TEXT,
+             locus            TEXT,
+             Id_seq           INTEGER   REFERENCES seq
+           );
+    """)
+
+    # seq_fragment  -- just an arbitrary fragment from some seq, for example: a BLAST  hit,
+    #                  or a region selected for phylogenetic analysis
     c.execute("""CREATE TABLE   seq_fragment
                      (
-                       Id      INTEGER PRIMARY KEY NOT NULL,
-                       seq_ori INTEGER  ,
+                       Id_frag INTEGER PRIMARY KEY NOT NULL,
+                       Id_seq  INTEGER REFERENCES seq,
                        beg     INT,             -- relative to the original
                        end     INT,
                        seq     TXT              -- The aligned sequence, with internal gaps -, but without external
-                     )
+                     );
               """)
 
-    # align
+    # align -- for axample a BLAST result from one query, or a "normal" Multialignment.
+    #          if all the sequences are experimental (not necessary bio_seq) it is a full_align (don't BLAST!)
     c.execute(""""CREATE TABLE    align
                      (
-                       Id    INTEGER PRIMARY KEY AUTOINCREMENT,
-                       Name  TEXT
-                     )
+                       Id_align  INTEGER PRIMARY KEY AUTOINCREMENT,
+                       Name      TEXT
+                       Id_file   INTEGER          REFERENCES seq_file,
+                     );
               """)
 
     # aligned_seq
     c.execute("""CREATE TABLE   aligned_seq
                      (
-                       Id      INTEGER PRIMARY KEY NOT NULL,
-                       seq_ori INTEGER ,
-                       seq_frg INTEGER ,
-                       align   INTEGER NOT NULL,
-                       beg     INT,             -- relative to the alignment
-                       end     INT,
-                       seq     TXT              -- The aligned sequence, with internal gaps -, but without external
+                       Id_algseq  INTEGER PRIMARY KEY NOT NULL,
+                       Id_seq     INTEGER          REFERENCES seq,
+                       Id_frag    INTEGER          REFERENCES seq_fragment,
+                       Id_align   INTEGER NOT NULL REFERENCES align,
+                       beg        INT,             -- relative to the alignment
+                       end        INT,
+                       seq        TXT              -- The aligned sequence, with internal gaps -, but without external
                      )
               """)
 
     # strain
     c.execute("""CREATE TABLE   strain
            (
-             Id    INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name  TEXT    UNIQUE
+             Id_strain  INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name       TEXT    UNIQUE
            )
     """)
 
     # isolate
     c.execute("""CREATE TABLE   isolate
            (
-             Id     INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name   TEXT,
-             strain INTEGER NOT NULL,
-             date   TEXT,
-             year   INT,
-             month  INT,
-             day    INT,
-             host   INTEGER NOT NULL,     -- original taxa
-             source INTEGER NOT NULL,
-             author INTEGER NOT NULL,
-             institution INTEGER NOT NULL,
-             location    INTEGER NOT NULL,
+             Id_isolate  INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name        TEXT,
+             Id_strain   INTEGER NOT NULL,
+             date        TEXT,
+             year        INT,
+             month       INT,
+             day         INT,
+             Id_host     INTEGER NOT NULL,     -- original taxa
+             Id_source   INTEGER NOT NULL,
+             Id_author   INTEGER NOT NULL,
+             Id_institution INTEGER NOT NULL,
+             Id_location    INTEGER NOT NULL,
              coordinate  TXT,
            )
     """)
@@ -96,37 +123,37 @@ def create() -> sqlite3.Connection:
     # author
     c.execute("""CREATE TABLE   author
            (
-             Id       INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name     TEXT    ,
-             middle   TEXT ,
-             surname  TEXT ,
-             title    TEXT,
-             genre    TEXT,
-             contact  INTEGER NOT NULL
+             Id_author  INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name       TEXT    ,
+             middle     TEXT ,
+             surname    TEXT ,
+             title      TEXT,
+             genre      TEXT,
+             Id_contact INTEGER NOT NULL
            )
     """)
 
     # institution
     c.execute("""CREATE TABLE   institution
            (
-             Id       INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name     TEXT    ,
-             category TEXT ,
-             parent   INTEGER NOT NULL,
-             contact  INTEGER NOT NULL,
+             Id_institution  INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name            TEXT    ,
+             category        TEXT ,                    -- Institute, Department, University, Laboratory, Center, etc.
+             parent          INTEGER NOT NULL,
+             Id_contact      INTEGER NOT NULL,
            )
     """)
 
     # affiliation
     c.execute("""CREATE TABLE   affiliation
            (
-             author        INTEGER NOT NULL,
-             institution   INTEGER NOT NULL,
+             Id_author        INTEGER NOT NULL,
+             Id_institution   INTEGER NOT NULL,
              beg_year      INT,
              beg_month     INT,
              end_year      INT,
              end_month     INT,
-             contact       INTEGER NOT NULL,
+             Id_contact    INTEGER NOT NULL,
              position      TEXT
 
            )
@@ -135,21 +162,22 @@ def create() -> sqlite3.Connection:
     # genomic_region
     c.execute("""CREATE TABLE   genomic_region
            (
-             Id    INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name  TEXT    ,
-             taxa  INTEGER NOT NULL,
-             beg   INT,
-             end   INT
+             Id_gen   INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name     TEXT    ,
+             Id_taxa  INTEGER NOT NULL REFERENCES taxa,
+             beg      INT,
+             end      INT,
+             Id_seq   INTEGER   REFERENCES seq,   -- just to map the positions, preferably a bio_seq
            )
     """)
 
     # source
     c.execute("""CREATE TABLE   source
            (
-             host        INTEGER NOT NULL,     -- original taxa
-             artificial  TEXT ,                -- NULL, clone, cell, patent, etc.
-             organ       TEXT,                 -- lever, serum, faces, etc.
-             environment TEXT                  -- NULL, seawater,  etc.
+             Id_host        INTEGER NOT NULL,     -- original taxa
+             artificial     TEXT ,                -- NULL, clone, cell, patent, etc.
+             organ          TEXT,                 -- lever, serum, faces, etc.
+             environment    TEXT                  -- NULL, seawater,  etc.
             )
     """)
 
@@ -168,50 +196,60 @@ def create() -> sqlite3.Connection:
     # country
     c.execute("""CREATE TABLE   country
            (
-             Id        INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name      TEXT  UNIQUE  ,
-             idx       TEXT  UNIQUE ,
-             region    TEXT,
-             continent TEXT
+             Id_country INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name       TEXT  UNIQUE  ,
+             idx        TEXT  UNIQUE ,
+             region     TEXT,
+             continent  TEXT
            )
     """)
 
     # taxa
     c.execute("""CREATE TABLE   taxa
            (
-             Id       INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name     TEXT    ,
-             vulgar   TEXT ,
-             parent   INTEGER NOT NULL,
-             category INTEGER NOT NULL,
-             NCBI     TXT
+             Id_taxa     INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name        TEXT    ,
+             vulgar      TEXT ,
+             parent      INTEGER NOT NULL,
+             Id_category INTEGER NOT NULL,
+             NCBI        TXT
            )
     """)
 
     # taxa_category
     c.execute("""CREATE TABLE   taxa_category
            (
-             Id       INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name     TEXT    UNIQUE,
+             Id_category   INTEGER PRIMARY KEY AUTOINCREMENT,
+             Name          TEXT    UNIQUE,
+             parent        INTEGER NOT NULL,
+             NCBI          TXT
+           )
+    """)
+
+    # to_excel  VIEW
+    c.execute("""CREATE VIEW TABLE   to_excel   AS SELECT
+           (
+             -- Id       INTEGER PRIMARY KEY AUTOINCREMENT,
+             Seq_Name     TEXT    UNIQUE,
              parent   INTEGER NOT NULL,
              NCBI     TXT
            )
     """)
 
-    # big VIEW
-    c.execute("""CREATE VIEW TABLE   big   AS SELECT
-           (
-             Id       INTEGER PRIMARY KEY AUTOINCREMENT,
-             Name     TEXT    UNIQUE,
-             parent   INTEGER NOT NULL,
-             NCBI     TXT
-           )
-    """)
+    # to_doc
+
 
     return sdb
 
 
-def parseAlign( file_name=None):
+def parse_full_Align( file_name=None):
+    """Will parse an alignment and insert it into the tables:
+        files: the file path ,
+        align:
+        seq: because assume this sequences are all complete original sequences (but may be a partial sequence from
+             some isolate ! )
+
+        """
     if not file_name:
         file_name = filedialog.askopenfilename(filetypes=(("fasta aligment", "*.fas"), ("All files", "*.*")),
                                                defaultextension='fas',
@@ -222,7 +260,11 @@ def parseAlign( file_name=None):
     # self.refSeq.clear()
     print(file_name)
     c = sdb.cursor()
-    c.execute("INSERT INTO align VALUES (?,?)", (1, file_name))
+    c.execute("INSERT INTO seq_file (path) VALUES (?)", (file_name,))
+    Id_file = c.lastrowid()
+    c.execute("INSERT INTO align (Id_file, Name) VALUES (?, ?)", (Id_file, file_name))
+    Id_align = c.lastrowid()
+
     with open(file_name) as align_file:
         seq_name = ''
         seq_num = 0
@@ -251,7 +293,7 @@ def parseAlign( file_name=None):
                 print('Seq: ' + seq_name + str((seq_beg, seq_end)) + ": " + seq)
 
                 # CREATE TABLE seq(                            Id INT, Name TEXT, Seq TEXT, Len INT)")
-                c.execute("INSERT INTO seq VALUES (?,?,?,?)", (seq_num, seq_name, seq, len(seq)))
+                c.execute("INSERT INTO seq (Id, Name, Seq, Len) VALUES (?,?,?,?)", (seq_num, seq_name, seq, len(seq)))
 
                 # self.refSeq[seq_name] = Seq_pos(seq_beg, seq_end)
     sdb.commit()
